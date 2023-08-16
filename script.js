f5301033c2a27ae08a7899e3e3d39570052325db
robby <5150reb@gmail.com>
2023-08-16 16:55:40 -0400

    prompt for sizing, adjusts divs to fit container
    this was hard, thought about and researched for a few hours and couldn't find anything. i ended up looking on to another project and found '${}' which I was unaware of/forgotten how it worked. i was able to get an expression in though like i wanted and auto-adjust the size of each etchDiv with it.

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 //create constants
 const bigContainer = document.querySelector('.big-container');
 
-
 //create functions
+function createEtchDivs() {
+     let etchDivNumber = parseInt(prompt("Enter desired number of squares per side (1-100): "));
+     if (etchDivNumber > 100 || etchDivNumber < 1) {
+          alert("Invalid input. Refresh the page.");
+     } else {
 
-//something about this not working, divs create but do not respond to hover
-for (i = 0; i < 256; i++) {
-     let etchDiv = document.createElement('div');
-     bigContainer.appendChild(etchDiv);
-     etchDiv.classList.add('etch-div'); 
-     etchDiv.addEventListener('mouseover', () => {
-          etchDiv.classList.add('hover');
-     })
-}
+          for (i = 0; i < etchDivNumber**2; i++) {
+               let etchDiv = document.createElement('div');
+               let etchDivWidth = 600/(etchDivNumber);
+               let etchDivHeight = etchDivWidth;
+               const widthHeightAttribute = document.createAttribute("style");
+               widthHeightAttribute.value = `width: ${etchDivWidth}px; height: ${etchDivHeight}px;`;
+               etchDiv.setAttributeNode(widthHeightAttribute);
+               bigContainer.appendChild(etchDiv);
+               etchDiv.classList.add('etch-div'); 
+               etchDiv.addEventListener('mouseover', () => {
+                    etchDiv.classList.add('hover');
+               })
+          }          
+     }
+     
+};
 
-//create event listeners
-
-
-//call functions
-//createEtchDivs();
+createEtchDivs();
