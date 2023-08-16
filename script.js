26027697976b8e7b6eefea91577498d262dcfd97
robby <5150reb@gmail.com>
2023-08-16 17:10:17 -0400

    added button to start sketch, bugs for multiple
    button works to generate first sketch, creates subsequent boards below first board for multiple sketches

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 //create constants
 const bigContainer = document.querySelector('.big-container');
+const generatorButton = document.querySelector('.generator');
 
 //create functions
+
+//this function is a functional mess
 function createEtchDivs() {
      let etchDivNumber = parseInt(prompt("Enter desired number of squares per side (1-100): "));
      if (etchDivNumber > 100 || etchDivNumber < 1) {
           alert("Invalid input. Refresh the page.");
      } else {
-
           for (i = 0; i < etchDivNumber**2; i++) {
                let etchDiv = document.createElement('div');
                let etchDivWidth = 600/(etchDivNumber);
@@ -25,4 +27,8 @@ function createEtchDivs() {
      
 };
 
-createEtchDivs();
+generatorButton.addEventListener('click', () => {
+     createEtchDivs();
+})
+
+
