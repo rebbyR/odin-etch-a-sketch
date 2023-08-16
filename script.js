8b1732f316e11dbe09bf0cae494ee04b8f860aa2
robby <5150reb@gmail.com>
2023-08-16 13:33:18 -0400

    achieved 16x16 grid of etch-divs
    forced height of big-container to be w: 1700px and h: 1600px -- likely have to be adjusted after border removal (if borders are removed)

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const bigContainer = document.querySelector('.big-container');
 
 
 function createEtchDivs() {
-    for (i = 0; i < 16; i++) {
+    for (i = 0; i < 256; i++) {
         const etchDiv = document.createElement('div');
         etchDiv.classList.add('etch-div');
         bigContainer.appendChild(etchDiv);
